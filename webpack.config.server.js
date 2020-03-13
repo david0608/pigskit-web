f1546a1275c3b58a535cb4b1ac0017e6054e78b3
davidwu <david6906817@gmail.com>
2020-03-13 23:19:04 +0800

    bundle express

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,5 +1,6 @@
 const path = require('path')
-const nodeExternals = require('webpack-node-externals')
+const webpack = require('webpack')
+const resolve = require('path').resolve
 
 module.exports = (env, argv) => {
     return ({
@@ -14,15 +15,12 @@ module.exports = (env, argv) => {
         mode: argv.mode,
         target: 'node',
         node: {
-            // Need this when working with express, otherwise the build fails
-            __dirname: false,   // if you don't put this is, __dirname
-            __filename: false,  // and __filename return blank or /
+            __dirname: false,
+            __filename: false,
         },
-        externals: [nodeExternals()], // Need this to avoid error when working with Express
         module: {
             rules: [
                 {
-                    // Transpiles ES6-8 into ES5
                     test: /\.js$/,
                     exclude: /node_modules/,
                     use: {
@@ -30,6 +28,15 @@ module.exports = (env, argv) => {
                     }
                 }
             ]
-        }
+        },
+        plugins: [
+            new webpack.ContextReplacementPlugin(
+              /express\/lib/,
+              resolve('node_modules'),
+              {
+                'ejs': 'ejs'
+              }
+            )
+          ]
     })
 }
