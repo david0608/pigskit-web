8894b3151328e276208b68ec5a4c04d3d2203262
davidwu <david6906817@gmail.com>
2020-03-16 16:55:51 +0800

    server check letsencrypt, self-signed or none.

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,13 @@
 import fs from 'fs';
 import express from 'express';
+import http from 'http';
 import https from 'https';
 import httpProxy from 'http-proxy';
 import * as path from 'path';
 
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT;
 const app = express();
 const proxy = httpProxy.createProxyServer({});
-
-const privateKey = fs.readFileSync(path.join(__dirname, '../certificate/privkey.pem'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, '../certificate/cert.pem'), 'utf8');
-const ca = fs.readFileSync(path.join(__dirname, '../certificate/chain.pem'), 'utf8');
-
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca,
-};
 
 proxy.on('error', function (err, req, res) {
     console.log(err)
@@ -53,6 +44,33 @@ app.get('/home', (req, res) => {
     proxy.web(req, res, { target: 'http://0.0.0.0:3000/' });
 });
 
-https.createServer(credentials, app).listen(PORT, () => {
-    console.log(`Server listening port : ${PORT}`);
-})
+console.log('Trying to start as https server...');
+try {
+    const privateKey = fs.readFileSync(path.join(__dirname, '../certificate/privkey.pem'), 'utf8');
+    const certificate = fs.readFileSync(path.join(__dirname, '../certificate/cert.pem'), 'utf8');
+    console.log('Found server key and certificate.');
+    console.log('Trying to start with letsencrypt certificate...');
+
+    try {
+        const ca = fs.readFileSync(path.join(__dirname, '../certificate/chain.pem'), 'utf8');
+        console.log('Found ca info.');
+        const credentials = {
+            key: privateKey,
+            cert: certificate,
+            ca: ca,
+        };
+        https.createServer(credentials, app).listen(PORT || 443, () => console.log(`Production https server listening port: ${PORT || 443}.`));
+    } catch(error) {
+        console.log(error);
+        console.log('No ca info found.');
+        const credentials = {
+            key: privateKey,
+            cert: certificate,
+        };
+        https.createServer(credentials, app).listen(PORT || 443, () => console.log(`Development https server listening port: ${PORT || 443}.`));
+    }
+} catch(error) {
+    console.log(error);
+    console.log('No server key or certificate found.');
+    http.createServer(app).listen(PORT || 80, () => console.log(`Http server listening port: ${PORT || 80}.`));
+}
