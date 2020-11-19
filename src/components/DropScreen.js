b7e4b389e53d1e6eb2ed051aa6a0a3c996e10e9c
David Wu <david6906817@gmail.com>
2020-11-20 00:47:28 +0800

    Rebuild DropScreen using styled-components.

--- src/components/DropScreen.js
+++ src/components/DropScreen.js
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
+import styled from 'styled-components'
 import clsx from 'clsx'
 import { BsX } from 'react-icons/bs'
-import CircButton from '../CircButton'
-import './index.less'
+import { withClass } from './utils'
+import CircButton from './CircButton'
 
 const Context = React.createContext()
 
@@ -24,6 +25,63 @@ const DropScreenProvider = React.memo((props) => {
         </Context.Provider>
     )
 })
+
+const DropScreenShadow = withClass(
+    'DropScreen-shadow',
+    styled.div`
+        display: none;
+        position: fixed;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, .1);
+        z-index: 100;
+
+        &.Display {
+            display: block;
+        }
+    `,
+)
+
+const DropScreenRoot = withClass(
+    'DropScreen-root',
+    styled.div`
+        position: fixed;
+        left: 50%;
+        transform: translate(-50%, -300px);
+        background-color: white;
+        box-shadow: 0 2px 28px 0 rgba(0,0,0,0.12);
+        border: 1px solid rgba(0,0,0,0.2);
+        z-index: 101;
+        transition: unset;
+
+        @media screen and (max-width: 499px) {
+            width: 100%;
+        }
+
+        &.Display {
+            transition: transform .5s;
+            transform: translate(-50%, 50px);
+
+            @media screen and (max-width: 499px) {
+                transform: translate(-50%, 0px)
+            }
+        }
+
+        >.Close {
+            position: absolute;
+            right: 10px;
+            top: -12px;
+            z-index: 109;
+
+            @media screen and (max-width: 499px) {
+                right: 5px;
+                top: 5px;
+            }
+        }
+    `
+)
 
 class DropScreen extends React.PureComponent {
     constructor(props) {
@@ -58,12 +116,12 @@ class DropScreen extends React.PureComponent {
 
     render() {
         return (<>
-            <div
-                className={clsx('DropScreen-shadow', this.state.display && 'Display')}
+            <DropScreenShadow
+                className={clsx(this.state.display && 'Display')}
                 onClick={this.close.bind(this)}
             />
-            <div
-                className={clsx('DropScreen-root', this.props.className, this.state.display && 'Display')}
+            <DropScreenRoot
+                className={clsx(this.props.className, this.state.display && 'Display')}
             >
                 <CircButton
                     className='Close'
@@ -73,7 +131,7 @@ class DropScreen extends React.PureComponent {
                     <BsX/>
                 </CircButton>
                 {this.state.content}
-            </div>
+            </DropScreenRoot>
         </>)
     }
 }
